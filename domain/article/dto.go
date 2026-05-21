7eb160248d764df732d95f3bf6dce560b785e864
useryesa9@gmail.com <useryesa9@gmail.com>
2026-05-21 11:53:04 +0700

    feat: mengubah validasi input json create article

--- domain/article/dto.go
+++ domain/article/dto.go
@@ -1,7 +1,7 @@
 package article
 
 type CreateArticleRequest struct {
-	Title   string `json:"title" binding:"required,max=225"`
-	Slug    string `json:"slug" binding:"required,max=225"`
+	Title   string `json:"title" binding:"required,max=255"`
+	Slug    string `json:"slug" binding:"required,max=255"`
 	Content string `json:"content" binding:"required"`
 }
